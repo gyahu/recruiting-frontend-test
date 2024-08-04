aa3e0075c5cde3c551ace63fd9791dfbef84add9
Tomás Perry Vives <gyahu@pop-os.localdomain>
2024-08-04 19:05:59 -0400

    Add notes display component

---
+++ src/presentation/notes/Notes.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import './Notes.css';
+import RadioButtonCard, { RadioButtonCardProps } from '../generics/radio/RadioButtonCard';
+import getNotesUseCase from '../../domain/invoices/get-notes-usecase';
+import { InvoiceDomainModel } from '../../domain/invoices/invoices';
+import { notesTexts } from "./texts";
+
+const mapInvoiceFor = (invoiceId: string) => (invoices: InvoiceDomainModel[]): RadioButtonCardProps[] => invoices.map(invoice => ({
+  title: invoice.id,
+  subtitle: invoice.organization,
+  description: invoice.amount.toString(),
+  details: invoiceId,
+}));
+
+function Notes({selectedBill, selectedNote, setSelectedNote}) {
+  const [notes, setNotes] = useState([] as RadioButtonCardProps[]); 
+  const [radioName, setRadioName] = useState('');
+  useEffect(() => {
+    getNotesUseCase(selectedBill.title).then(mapInvoiceFor(selectedBill.title)).then(setNotes);
+  }, [selectedBill, selectedNote]);
+  useEffect(() => {
+    setRadioName(selectedBill.title);
+  }, [selectedBill]);
+  const onClick = (note) => () => setSelectedNote(note);
+  return notes.length > 0 ? (
+    <div className="Notes">
+      <p> { notesTexts.title } </p>
+      <br/>
+      <div className="Notes-options">
+        {notes.map(note => (
+          <RadioButtonCard
+            invoice={ note }
+            selected={ selectedNote && selectedNote.title === note.title }
+            onClick={onClick(note)}
+            key={note.title}
+            name={radioName}/>
+        ))}
+      </div>
+    </div>
+  ) : <p> { notesTexts.empty } </p>;
+}
+
+export default Notes;
---
+++ src/presentation/notes/texts.ts
@@ -0,0 +1,4 @@
+export const notesTexts = {
+  title: 'Selecciona una nota de crédito',
+  empty: 'No hay notas asociadas a esta factura',
+};
