b73ae309336cace9734e7e666e9d6ba86608e151
Tomás Perry Vives <gyahu@pop-os.localdomain>
2024-08-04 19:33:58 -0400

    Update props on AssignResult

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
   return (
     <div className="App">
       <Modal show={show}>
-        <AssignResult handleClose={reset}/>
+        {selectedNote && <AssignResult handleClose={reset} assignedNote={selectedNote}/>}
       </Modal>
       <div className="App-header">
         <Billings
