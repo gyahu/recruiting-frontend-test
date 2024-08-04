8647319b8fe9bf9118658fc264a25e69651ea092
Tomás Perry Vives <gyahu@pop-os.localdomain>
2024-08-04 19:43:51 -0400

    Fix empty storage access

--- src/data/resource-manager.ts/storage.ts
+++ src/data/resource-manager.ts/storage.ts
@@ -3,7 +3,7 @@ const getLastUpdateOn = (key: string): Date | undefined => {
   if (!localStorage.getItem(`${key}-last-update`)) return undefined;
   return new Date(localStorage.getItem(`${key}-last-update`) ?? '');
 }
-const getValuesIn = <T>(key: string): T | undefined => JSON.parse(localStorage.getItem(key)?? '')[key] || undefined;
+const getValuesIn = <T>(key: string): T | undefined => JSON.parse(localStorage.getItem(key) ?? '{}')[key] || undefined;
 const saveTo = <T>(key: string, value: T) => {
   localStorage.setItem(key, JSON.stringify({ [key]: value }));
   localStorage.setItem(`${key}-last-update`, new Date().toISOString());
