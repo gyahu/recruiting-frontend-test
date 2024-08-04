e386120f5756c6810ffd1504adabdb26a31f64af
Tomás Perry Vives <gyahu@pop-os.localdomain>
2024-08-04 19:48:02 -0400

    Add logic to filter locally assigned notes

--- src/domain/invoices/get-notes-usecase.ts
+++ src/domain/invoices/get-notes-usecase.ts
@@ -1,28 +1,30 @@
 import { AxiosResponse } from "axios";
-import { getPendingInvoices } from "../../data/invoices/repository"
+import { getPendingInvoices, loadAssignedInvoices, loadPendingInvoices } from "../../data/invoices/repository"
 import { mapToDomainModel } from "./mapper";
 import { InvoiceDomainModel } from "./invoices";
-import { getLastUpdateOn, getValuesIn } from "../../data/resource-manager.ts/storage";
+import { getLastUpdateOn } from "../../data/resource-manager.ts/storage";
 import { Invoice } from "../../data/invoices/invoices";
 
 const isSuccess = (response: AxiosResponse<unknown>) => Math.trunc(response.status / 100) === 2;
 
-const parseInvoices = (invoices: Invoice[], invoiceId: string) => invoices.filter(invoice =>
+const parseInvoicesWithout = (locallyAssignedInvoicesIds: string[]) => (invoices: Invoice[], invoiceId: string) => invoices.filter(invoice =>
   invoice.type === 'credit_note'
   && invoice.reference
   && invoice.reference === invoiceId
+  && !locallyAssignedInvoicesIds.includes(invoice.id)
 ).map(mapToDomainModel)
 
 const getNotesUseCase = async (invoiceId: string): Promise<InvoiceDomainModel[]> => {
-  const localInvoices = getValuesIn<Invoice[]>('pendingInvoices') ?? [];
+  const locallyAssignedInvoices = loadAssignedInvoices();
+  const localInvoices = loadPendingInvoices();
   const lastUpdate = getLastUpdateOn('pendingInvoices');
   const secondsSinceLastUpdate = lastUpdate ? (new Date().getTime() - lastUpdate.getTime()) / 1000 : Infinity;
   if (localInvoices.length > 0 && secondsSinceLastUpdate < 60) {
-    return parseInvoices(localInvoices, invoiceId);
+    return parseInvoicesWithout(locallyAssignedInvoices)(localInvoices, invoiceId);
   }
   const response = await getPendingInvoices();
   if (isSuccess(response)) {
-    return parseInvoices(response.data, invoiceId);
+    return parseInvoicesWithout(locallyAssignedInvoices)(response.data, invoiceId);
   }
   return [];
 }
